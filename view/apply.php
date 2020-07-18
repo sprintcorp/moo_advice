eb1796825fc7182826a92118c331ae44cdda3d6e
Frederick Adebayo <37757522+sprintcorp@users.noreply.github.com>
2020-07-18 10:33:10 -0700

    Update apply.php
    Added list of states to view/apply.php

--- view/apply.php
+++ view/apply.php
@@ -115,7 +115,43 @@
                                     <label for="inputState">State</label>
                                     <select id="inputState" name="state" class="form-control" required>
                                         <option selected>Choose...</option>
-                                        <option>...</option>
+                                        <option>ABUJA FCT</option>
+                                        <option>ABIA</option>
+                                        <option>ADAMAWA</option>
+                                        <option>AKWA IBOM</option>
+                                        <option>ANAMBRA</option>
+                                        <option>BAUCHI</option>
+                                        <option>BAYELSA</option>
+                                        <option>BENUE</option>
+                                        <option>BORNO</option>
+                                        <option>CROSS RIVER</option>
+                                        <option>DELTA</option>
+                                        <option>EBONYI</option>
+                                        <option>EDO</option>
+                                        <option>EKITI</option>
+                                        <option>ENUGU</option>
+                                        <option>GOMBE</option>
+                                        <option>IMO</option>
+                                        <option>JIGAWA</option>
+                                        <option>KADUNA</option>
+                                        <option>KANO</option>
+                                        <option>KATSINA</option>
+                                        <option>KEBBI</option>
+                                        <option>KOGI</option>
+                                        <option>KWARA</option>
+                                        <option>LAGOS</option>
+                                        <option>NASSARAWA</option>
+                                        <option>NIGER</option>
+                                        <option>OGUN</option>
+                                        <option>ONDO</option>
+                                        <option>OSUN</option>
+                                        <option>OYO</option>
+                                        <option>PLATEAU</option>
+                                        <option>RIVERS</option>
+                                        <option>SOKOTO</option>
+                                        <option>TARABA</option>
+                                        <option>YOBE</option>
+                                        <option>ZAMFARA</option>
                                     </select>
 
                                 </div>
